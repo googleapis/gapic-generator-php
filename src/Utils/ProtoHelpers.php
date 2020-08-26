432edc86c422908a89014976220e3a384f11ccf6
Chris Bacon <chris@dunelm.org.uk>
2020-08-26 20:31:34 +0100

    Code to read custom proto options, plus test (#8)
    PHP does not support reading custom options directly, so these helper functions are required

--- src/Utils/ProtoHelpers.php
+++ src/Utils/ProtoHelpers.php
@@ -2,7 +2,12 @@
 
 namespace Google\Generator\Utils;
 
+use \Google\Generator\Collections\Vector;
+use \Google\Protobuf\Internal\CodedInputStream;
 use \Google\Protobuf\Internal\FileDescriptorProto;
+use \Google\Protobuf\Internal\GPBWire;
+use \Google\Protobuf\Internal\HasPublicDescriptorTrait;
+use \Google\Protobuf\Internal\Message;
 
 class ProtoHelpers
 {
@@ -38,5 +43,96 @@ class ProtoHelpers
     {
         $desc->underlyingProto = $proto;
         return $desc;
+    }
+
+    // Return type is dependant on option type. Either string, int, or Vector of string or int,
+    // or null if not repeated and value doesn't exist. Repeated returns empty vector if not exists.
+    private static function getCustomOptionRaw(Message $message, int $optionId, bool $repeated)
+    {
+        static $messageUnknown;
+        if (!$messageUnknown)
+        {
+            $ref = new \ReflectionClass('Google\Protobuf\Internal\Message');
+            $messageUnknown = $ref->getProperty('unknown');
+            $messageUnknown->setAccessible(true);
+        }
+
+        $values = [];
+        if ($message->hasOptions())
+        {
+            $opts = $message->getOptions();
+            $unknown = $messageUnknown->getValue($opts);
+            if ($unknown)
+            {
+                $unknownStream = new CodedInputStream($unknown);
+                // Read through the stream of all custom options, looking for
+                // the requested option-id. If it's repeated, then all options
+                // must be parsed, otherwise return the first found.
+                while (($tag = $unknownStream->readTag()) !== 0)
+                {
+                    $value = 0;
+                    // TODO: Handle extra option types as required.
+                    switch (GPBWire::getTagWireType($tag)) {
+                        case GPBWire::WIRETYPE_VARINT:
+                            $unknownStream->readVarint32($value);
+                            break;
+                        case GPBWire::WIRETYPE_LENGTH_DELIMITED:
+                            $len = 0;
+                            $unknownStream->readVarintSizeAsInt($len);
+                            $unknownStream->readRaw($len, $value);
+                            break;
+                        default:
+                            throw new \Exception('Cannot read option tag');
+                    }
+                    if (GPBWire::getTagFieldNumber($tag) === $optionId) {
+                        if ($repeated) {
+                            $values[] = $value;
+                        } else {
+                            return $value;
+                        }
+                    }
+                }
+            }
+        }
+        return $repeated ? Vector::new($values) : null;
+    }
+
+    private static function conformMessage($message): Message
+    {
+        if (isset($message->underlyingProto)) {
+            $message = $message->underlyingProto;
+        }
+        if (!($message instanceof Message)) {
+            throw new \Exception('Can only get custom option of Message or HasPublicDescriptorTrait');
+        }
+        return $message;
+    }
+
+    /**
+     * Get a non-repeated custom option. The option can be of any type, which determines the return type.
+     * 
+     * @param mixed $message The message containing the custom option. Must be of type Message, or
+     *     a descriptor with an `underlyingProto` property.
+     * @param int $optionId The option-id of the option to get.
+     * 
+     * @return mixed Will be null if the option does not exist.
+     */
+    public static function getCustomOption($message, int $optionId)
+    {
+        return static::getCustomOptionRaw(static::conformMessage($message), $optionId, false);
+    }
+
+    /**
+     * Get a repeated custom option. The option can be of any type, which determines to content of the returned Vector.
+     * 
+     * @param mixed $message The message containing the custom option. Must be of type Message, or
+     *     a descriptor with an `underlyingProto` property.
+     * @param int $optionId The option-id of the option to get.
+     * 
+     * @return Vector Will be an empty Vector if the option does not exist.
+     */
+    public static function getCustomOptionRepeated($message, int $optionId): Vector
+    {
+        return static::getCustomOptionRaw(static::conformMessage($message), $optionId, true);
     }
 }
---
+++ tests/Utils/ProtoHelpersTest.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types=1);
+
+namespace Google\Generator\Tests\Collections;
+
+use PHPUnit\Framework\TestCase;
+use Google\Generator\Collections\Vector;
+use Google\Generator\Utils\ProtoHelpers;
+use Google\Protobuf\Internal\FileDescriptorSet;
+
+final class ProtoHelpersTest extends TestCase
+{
+    public function testCustomOptions(): void
+    {
+        // TODO: Extract this code to invoke protoc and load descriptors, when more tests require it.
+
+        // Set up required file locations and create tmp output file for protoc invocation.
+        $cwd = getcwd();
+        $protoc = "{$cwd}/tools/protoc";
+        $protobuf = "{$cwd}/protobuf/src/";
+        $descRes = tmpfile();
+        $descFilename = stream_get_meta_data($descRes)['uri'];
+        $input = "{$cwd}/tests/Utils/custom_options.proto";
+        // Invoke protoc to build the descriptor of the test proto.
+        $protocCmdLine = "{$protoc} --include_imports --include_source_info -o {$descFilename} -I {$protobuf} -I {$cwd} {$input} 2>&1";
+        $output = [];
+        $result = -1;
+        exec($protocCmdLine, $output, $result);
+        // Fail with helpful error message if protoc failed.
+        $this->assertEquals(0, $result, implode("\n", $output));
+
+        // Load the proto descriptors.
+        $descBytes = stream_get_contents($descRes);
+        $descSet = new FileDescriptorSet();
+        $descSet->mergeFromString($descBytes);
+        // Select the correct file from the descriptor-set.
+        $file = Vector::new($descSet->getFile())->filter(fn($x) => $x->getName() === 'tests/Utils/custom_options.proto')[0];
+
+        // Check custom options are loaded successfully.
+        $this->assertEquals(42, ProtoHelpers::getCustomOption($file, 2000));
+        $this->assertEquals('stringy', ProtoHelpers::getCustomOption($file, 2001));
+        $this->assertEquals([8, 9, 10], ProtoHelpers::getCustomOptionRepeated($file, 2002)->toArray());
+        $this->assertEquals(['s1', 's2'], ProtoHelpers::getCustomOptionRepeated($file, 2003)->toArray());
+    }
+}
