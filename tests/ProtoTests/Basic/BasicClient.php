836b3d121784007ea7b0ed727f8c357c6113ad2b
Chris Bacon <chris@dunelm.org.uk>
2020-09-29 03:39:29 -0400

    Tests use files to define expected generated code

---
+++ tests/ProtoTests/Basic/BasicClient.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace testing\basic;
+
+use testing\basic\Gapic\BasicGapicClient;
+
+/** {@inheritdoc} */
+class BasicClient extends BasicGapicClient
+{
+    // This class is intentionally empty, and is intended to hold manual additions to
+    // the generated {@see BasicGapicClient} class.
+}
