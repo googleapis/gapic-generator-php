2dfb631514b2f534f2c980a61105b2303ac77f4f
Chris Bacon <chris@dunelm.org.uk>
2020-09-30 08:02:35 -0400

    Generate two more test helpers
    An example of a generated PHP test file: https://github.com/googleapis/google-cloud-php/blob/a879c61ac550cc12c501a8a822f6141a78653ff4/Dlp/tests/Unit/V2/DlpServiceClientTest.php

--- src/Generation/UnitTestsGenerator.php
+++ src/Generation/UnitTestsGenerator.php
@@ -18,6 +18,7 @@ declare(strict_types=1);
 
 namespace Google\Generator\Generation;
 
+use Google\ApiCore\CredentialsWrapper;
 use Google\ApiCore\Testing\GeneratedTest;
 use Google\ApiCore\Testing\MockTransport;
 use Google\ApiCore\Transport\TransportInterface;
@@ -57,7 +58,9 @@ class UnitTestsGenerator
     private function GenerateClass(): PhpClass
     {
         return AST::class($this->serviceDetails->unitTestsType, $this->ctx->type(Type::fromName(GeneratedTest::class)))
-            ->withMember($this->createTransport());
+            ->withMember($this->createTransport())
+            ->withMember($this->createCredentials())
+            ->withMember($this->createClient());
     }
 
     private function createTransport(): PhpClassMember
@@ -71,6 +74,42 @@ class UnitTestsGenerator
             ))
             ->withPhpDoc(PhpDoc::block(
                 PhpDoc::return($this->ctx->type(Type::fromName(TransportInterface::class)))
+            ));
+    }
+
+    private function createCredentials(): PhpClassMember
+    {
+        return AST::method('createCredentials')
+            ->withAccess(Access::PRIVATE)
+            ->withBody(AST::block(
+                AST::return(
+                    AST::call(
+                        AST::call(
+                            AST::call(AST::THIS, AST::method('getMockBuilder'))(
+                                AST::access($this->ctx->type(Type::fromName(CredentialsWrapper::class)), AST::CLS)),
+                            AST::method('disableOriginalConstructor'))(),
+                        AST::method('getMock'))()
+                )
+            ))
+            ->withPhpDoc(PhpDoc::block(
+                PhpDoc::return($this->ctx->type(Type::fromName(CredentialsWrapper::class)))
+            ));
+    }
+
+    private function createClient(): PhpClassMember
+    {
+        $options = AST::param($this->ctx->type(Type::array()), AST::var('options'), AST::array([]));
+        return AST::method('createClient')
+            ->withAccess(Access::PRIVATE)
+            ->withParams($options)
+            ->withBody(AST::block(
+                AST::binaryOp($options->var, '+=', AST::array([
+                    'credentials' => AST::call(AST::THIS, $this->createCredentials())()
+                ])),
+                AST::return(AST::new($this->ctx->type($this->serviceDetails->emptyClientType))($options->var))
+            ))
+            ->withPhpDoc(PhpDoc::block(
+                PhpDoc::return($this->ctx->type($this->serviceDetails->emptyClientType))
             ));
     }
 }
