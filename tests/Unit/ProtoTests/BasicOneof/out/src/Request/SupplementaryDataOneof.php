b7c0d68ab6601e7e53578a9f50e3de48cf522f5f
Mira Leung <miraleung@users.noreply.github.com>
2021-07-22 10:33:17 -0700

    feat(oneofs): add private fields to the wrapper oneof class [ggp] (#326)
    * feat: initial scaffolding for required oneofs
    
    * feat: initial scaffolding for required oneofs
    
    * fix: update oneof wrapper class location and namespace
    
    * fix: update oneof wrapper location and namespace
    
    * feat: add private fields

--- tests/Unit/ProtoTests/BasicOneof/out/src/Request/SupplementaryDataOneof.php
+++ tests/Unit/ProtoTests/BasicOneof/out/src/Request/SupplementaryDataOneof.php
@@ -27,4 +27,44 @@ namespace Testing\BasicOneof\Request;
 /** Wrapper class for the oneof supplementary_data defined in message  */
 class SupplementaryDataOneof
 {
+    /**
+     * The value for the field extra_description, if set.
+     */
+    private static $extraDescription;
+
+    /**
+     * The value for the field extra_summary, if set.
+     */
+    private static $extraSummary;
+
+    /**
+     * The value for the field extra_request, if set.
+     */
+    private static $extraRequest;
+
+    /**
+     * The value for the field extra_index, if set.
+     */
+    private static $extraIndex;
+
+    /**
+     * The value for the field extra_double, if set.
+     */
+    private static $extraDouble;
+
+    /**
+     * The value for the field extra_float, if set.
+     */
+    private static $extraFloat;
+
+    /**
+     * The value for the field extra_bool, if set.
+     */
+    private static $extraBool;
+
+    /**
+     * Name of the field for which the oneof is set, as it appears in the protobuf in
+     * lower_camel_case.
+     */
+    private static $selectedOneofFieldName = '';
 }
