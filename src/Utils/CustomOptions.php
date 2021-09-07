43ae1ff265dcf3da8aad8cb9522911ab54f5a539
noahdietz <ndietz@google.com>
2021-09-07 09:00:30 -0700

    add link to original proto

--- src/Utils/CustomOptions.php
+++ src/Utils/CustomOptions.php
@@ -30,6 +30,7 @@ class CustomOptions
 
     public const GOOGLE_API_FIELDBEHAVIOR_REQUIRED = 2;
 
+    // Protobuf extension numbers from https://github.com/googleapis/googleapis/blob/master/google/cloud/extended_operations.proto.
     public const GOOGLE_CLOUD_OPERATION_FIELD = 1149;
     public const GOOGLE_CLOUD_OPERATION_REQUEST_FIELD = 1150;
     public const GOOGLE_CLOUD_OPERATION_RESPONSE_FIELD = 1151;
