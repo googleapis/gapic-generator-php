efb3c70de3daf4049824e58f4110cd36eb0ee2f0
noahdietz <ndietz@google.com>
2021-09-15 14:59:16 -0700

    add integration tests

--- tests/Integration/goldens/compute_small/src/V1/resources/addresses_descriptor_config.php
+++ tests/Integration/goldens/compute_small/src/V1/resources/addresses_descriptor_config.php
@@ -3,6 +3,34 @@
 return [
     'interfaces' => [
         'google.cloud.compute.v1.Addresses' => [
+            'Delete' => [
+                'longRunning' => [
+                    'additionalArgumentMethods' => [
+                        'getProject',
+                        'getRegion',
+                    ],
+                    'getOperationMethod' => 'get',
+                    'cancelOperationMethod' => null,
+                    'deleteOperationMethod' => null,
+                    'operationNameMethod' => 'getName',
+                    'operationStatusMethod' => 'getStatus',
+                    'operationStatusDoneValue' => \Google\Cloud\Compute\V1\Operation\Status::DONE,
+                ],
+            ],
+            'Insert' => [
+                'longRunning' => [
+                    'additionalArgumentMethods' => [
+                        'getProject',
+                        'getRegion',
+                    ],
+                    'getOperationMethod' => 'get',
+                    'cancelOperationMethod' => null,
+                    'deleteOperationMethod' => null,
+                    'operationNameMethod' => 'getName',
+                    'operationStatusMethod' => 'getStatus',
+                    'operationStatusDoneValue' => \Google\Cloud\Compute\V1\Operation\Status::DONE,
+                ],
+            ],
             'AggregatedList' => [
                 'pageStreaming' => [
                     'requestPageTokenGetMethod' => 'getPageToken',
