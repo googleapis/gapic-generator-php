8c9eacbb258beca6ab136badbfdd7889a85b0d01
Brent Shaffer <betterbrent@google.com>
2022-07-20 16:30:57 +0000

    fix iam test

--- tests/Integration/goldens/iam/src/V1/Gapic/IAMPolicyGapicClient.php
+++ tests/Integration/goldens/iam/src/V1/Gapic/IAMPolicyGapicClient.php
@@ -189,8 +189,9 @@ class IAMPolicyGapicClient
     }
 
     /**
-     * Gets the access control policy for a resource. Returns an empty policy
-    if the resource exists and does not have a policy set.
+     * Gets the access control policy for a resource.
+     * Returns an empty policy if the resource exists and does not have a policy
+     * set.
      *
      * Sample code:
      * ```
@@ -237,8 +238,8 @@ class IAMPolicyGapicClient
     }
 
     /**
-     * Sets the access control policy on the specified resource. Replaces
-    any existing policy.
+     * Sets the access control policy on the specified resource. Replaces any
+     * existing policy.
      *
      * Sample code:
      * ```
@@ -284,13 +285,13 @@ class IAMPolicyGapicClient
     }
 
     /**
-     * Returns permissions that a caller has on the specified resource. If the
-    resource does not exist, this will return an empty set of
-    permissions, not a NOT_FOUND error.
-
-    Note: This operation is designed to be used for building
-    permission-aware UIs and command-line tools, not for authorization
-    checking. This operation may "fail open" without warning.
+     * Returns permissions that a caller has on the specified resource.
+     * If the resource does not exist, this will return an empty set of
+     * permissions, not a NOT_FOUND error.
+     *
+     * Note: This operation is designed to be used for building permission-aware
+     * UIs and command-line tools, not for authorization checking. This operation
+     * may "fail open" without warning.
      *
      * Sample code:
      * ```
