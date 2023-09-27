542b650688d40f49ec46f202ba703bd4d1cf2da1
Brent Shaffer <betterbrent@google.com>
2023-09-27 09:18:48 -0700

    chore(tests): add composer script to  update all tests at once

--- tests/Unit/ProtoTests/GoldenUpdateMain.php
+++ tests/Unit/ProtoTests/GoldenUpdateMain.php
@@ -20,119 +20,134 @@ namespace Google\Generator\Tests\Unit\ProtoTests;
 
 use Google\Generator\Utils\MigrationMode;
 
-require __DIR__ . '../../../../vendor/autoload.php';
-error_reporting(E_ALL);
+class GoldenUpdateMain
+{
+    private const UNIT_TESTS = [
+        1 => [
+            'name' => 'Basic',
+            'protoPath' => 'Basic/basic.proto',
+            'migrationMode' => MigrationMode::NEW_SURFACE_ONLY
+        ],
+        2 => [
+            'name' => 'BasicLro',
+            'protoPath' => 'BasicLro/basic-lro.proto'
+        ],
+        3 => [
+            'name' => 'BasicOneof',
+            'protoPath' => 'BasicOneof/basic-oneof.proto'
+        ],
+        4 => [
+            'name' => 'BasicPaginated',
+            'protoPath' => 'BasicPaginated/basic-paginated.proto'
+        ],
+        5 => [
+            'name' => 'BasicBidiStreaming',
+            'protoPath' => 'BasicBidiStreaming/basic-bidi-streaming.proto'
+        ],
+        6 => [
+            'name' => 'BasicServerStreaming',
+            'protoPath' => 'BasicServerStreaming/basic-server-streaming.proto'
+        ],
+        7 => [
+            'name' => 'BasicClientStreaming',
+            'protoPath' => 'BasicClientStreaming/basic-client-streaming.proto'
+        ],
+        8 => [
+            'name' => 'GrpcServiceConfig',
+            'protoPath' => 'GrpcServiceConfig/grpc-service-config1.proto',
+            'package' => 'testing.grpcserviceconfig'
+        ],
+        9 => [
+            'name' => 'RoutingHeaders',
+            'protoPath' => 'RoutingHeaders/routing-headers.proto',
+            'migrationMode' => MigrationMode::MIGRATING
+        ],
+        10 => [
+            'name' => 'DeprecatedService',
+            'protoPath' => 'DeprecatedService/deprecated_service.proto'
+        ],
+        11 => [
+            'name' => 'BasicDiregapic',
+            'protoPath' => 'BasicDiregapic/library_rest.proto',
+            'package' => 'google.example.library.v1',
+            'transport' => 'rest',
+            'migrationMode' => MigrationMode::PRE_MIGRATION_SURFACE_ONLY
+        ],
+        12 => [
+            'name' => 'ResourceNames',
+            'protoPath' => 'ResourceNames/resource-names.proto',
+            'migrationMode' => MigrationMode::MIGRATION_MODE_UNSPECIFIED
+        ],
+        13 => [
+            'name' => 'CustomLro',
+            'protoPath' => 'CustomLro/custom_lro.proto',
+            'package' => 'testing.customlro',
+            'transport' => 'rest'
+        ],
+        14 => [
+            'name' => 'DisableSnippets',
+            'protoPath' => 'DisableSnippets/disable_snippets.proto',
+            'package' => 'testing.disablesnippets',
+            'generateSnippets' => false
+        ],
+        15 => [
+            'name' => 'BasicOneof (new surface only)',
+            'protoPath' => 'BasicOneofNew/basic-oneof-new.proto',
+            'migrationMode' => MigrationMode::NEW_SURFACE_ONLY,
+        ],
+    ];
 
-const UNIT_TESTS = [
-  1 => [
-    'name' => 'Basic',
-    'protoPath' => 'Basic/basic.proto',
-    'migrationMode' => MigrationMode::NEW_SURFACE_ONLY
-  ],
-  2 => [
-    'name' => 'BasicLro',
-    'protoPath' => 'BasicLro/basic-lro.proto'
-  ],
-  3 => [
-    'name' => 'BasicOneof',
-    'protoPath' => 'BasicOneof/basic-oneof.proto'
-  ],
-  4 => [
-    'name' => 'BasicPaginated',
-    'protoPath' => 'BasicPaginated/basic-paginated.proto'
-  ],
-  5 => [
-    'name' => 'BasicBidiStreaming',
-    'protoPath' => 'BasicBidiStreaming/basic-bidi-streaming.proto'
-  ],
-  6 => [
-    'name' => 'BasicServerStreaming',
-    'protoPath' => 'BasicServerStreaming/basic-server-streaming.proto'
-  ],
-  7 => [
-    'name' => 'BasicClientStreaming',
-    'protoPath' => 'BasicClientStreaming/basic-client-streaming.proto'
-  ],
-  8 => [
-    'name' => 'GrpcServiceConfig',
-    'protoPath' => 'GrpcServiceConfig/grpc-service-config1.proto',
-    'package' => 'testing.grpcserviceconfig'
-  ],
-  9 => [
-    'name' => 'RoutingHeaders',
-    'protoPath' => 'RoutingHeaders/routing-headers.proto',
-    'migrationMode' => MigrationMode::MIGRATING
-  ],
-  10 => [
-    'name' => 'DeprecatedService',
-    'protoPath' => 'DeprecatedService/deprecated_service.proto'
-  ],
-  11 => [
-    'name' => 'BasicDiregapic',
-    'protoPath' => 'BasicDiregapic/library_rest.proto',
-    'package' => 'google.example.library.v1',
-    'transport' => 'rest',
-    'migrationMode' => MigrationMode::PRE_MIGRATION_SURFACE_ONLY
-  ],
-  12 => [
-    'name' => 'ResourceNames',
-    'protoPath' => 'ResourceNames/resource-names.proto',
-    'migrationMode' => MigrationMode::MIGRATION_MODE_UNSPECIFIED
-  ],
-  13 => [
-    'name' => 'CustomLro',
-    'protoPath' => 'CustomLro/custom_lro.proto',
-    'package' => 'testing.customlro',
-    'transport' => 'rest'
-  ],
-  14 => [
-    'name' => 'DisableSnippets',
-    'protoPath' => 'DisableSnippets/disable_snippets.proto',
-    'package' => 'testing.disablesnippets',
-    'generateSnippets' => false
-  ],
-  15 => [
-    'name' => 'BasicOneof (new surface only)',
-    'protoPath' => 'BasicOneofNew/basic-oneof-new.proto',
-    'migrationMode' => MigrationMode::NEW_SURFACE_ONLY,
-  ],
-];
+    public static function updateAll()
+    {
+        self::update(0);
+    }
 
-$optionString = implode("\n", array_map(
-    function ($v, $k) {
-        return sprintf("%s: '%s'", $k, $v['name']);
-    },
-    UNIT_TESTS,
-    array_keys(UNIT_TESTS)
-));
+    public static function update(int $selection)
+    {
+        require_once __DIR__ . '../../../../vendor/autoload.php';
+        error_reporting(E_ALL);
 
-$selection = -1;
-while ($selection < 0 || $selection > sizeof(array_keys(UNIT_TESTS))) {
-    print("============ Unit tests ==========\n$optionString\n\nSelect golden to update (0 for all): ");
-    $nextLine = fscanf(STDIN, "%d\n", $selection);
-}
+        $optionString = implode("\n", array_map(
+            function ($v, $k) {
+                return sprintf("%s: '%s'", $k, $v['name']);
+            },
+            self::UNIT_TESTS,
+            array_keys(self::UNIT_TESTS)
+        ));
 
-if ($selection !== 0) {
-    updateGolden($selection);
-} else {
-    foreach (array_keys(UNIT_TESTS) as $testIndex) {
-        updateGolden($testIndex);
+        while ($selection < 0 || $selection > sizeof(array_keys(self::UNIT_TESTS))) {
+            print("============ Unit tests ==========\n$optionString\n\nSelect golden to update (0 for all): ");
+            $nextLine = fscanf(STDIN, "%d\n", $selection);
+        }
+
+        if ($selection !== 0) {
+            self::updateGolden($selection);
+        } else {
+            foreach (array_keys(self::UNIT_TESTS) as $testIndex) {
+                self::updateGolden($testIndex);
+                print("\n");
+            }
+        }
+        return 0;
+    }
+
+    private static function updateGolden(int $testIndex)
+    {
+        $goldenUpdater = new UnitGoldenUpdater;
+        $testData = self::UNIT_TESTS[$testIndex];
+        print("Updating goldens for " . $testData['name'] . "\n");
+        $goldenUpdater->update(
+            $testData['protoPath'],
+            array_key_exists('package', $testData) ? $testData['package'] : null,
+            array_key_exists('transport', $testData) ? $testData['transport'] : null,
+            array_key_exists('generateSnippets', $testData) ? $testData['generateSnippets'] : true,
+            array_key_exists('migrationMode', $testData) ? $testData['migrationMode'] : MigrationMode::PRE_MIGRATION_SURFACE_ONLY
+        );
         print("\n");
     }
 }
-exit(0);
 
-function updateGolden(int $testIndex)
-{
-    $goldenUpdater = new UnitGoldenUpdater;
-    $testData = UNIT_TESTS[$testIndex];
-    print("Updating goldens for " . $testData['name'] . "\n");
-    $goldenUpdater->update(
-        $testData['protoPath'],
-        array_key_exists('package', $testData) ? $testData['package'] : null,
-        array_key_exists('transport', $testData) ? $testData['transport'] : null,
-        array_key_exists('generateSnippets', $testData) ? $testData['generateSnippets'] : true,
-        array_key_exists('migrationMode', $testData) ? $testData['migrationMode'] : MigrationMode::PRE_MIGRATION_SURFACE_ONLY
-    );
-    print("\n");
+if (isset($argv)) {
+    $selection = $argv[1] ?? -1;
+    GoldenUpdateMain::update($selection);
 }
