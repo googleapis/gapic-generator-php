b9ced301b68c754df48894103061d010a8b9870a
Brent Shaffer <bshafs@gmail.com>
2024-02-28 09:09:52 -0600

    fix: correct method for supportedTransports

--- tests/Unit/ProtoTests/CustomLro/out/src/Gapic/CustomLroGapicClient.php
+++ tests/Unit/ProtoTests/CustomLro/out/src/Gapic/CustomLroGapicClient.php
@@ -128,8 +128,8 @@ class CustomLroGapicClient
         return 'rest';
     }
 
-    /** Implements GapicClientTrait::getSupportedTransports. */
-    private static function getSupportedTransports()
+    /** Implements GapicClientTrait::supportedTransports. */
+    private static function supportedTransports()
     {
         return [
             'rest',
