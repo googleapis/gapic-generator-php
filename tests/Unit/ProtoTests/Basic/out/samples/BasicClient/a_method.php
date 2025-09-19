775d85b8804fabede596e34bb1c3996785f96289
Brent Shaffer <betterbrent@google.com>
2025-09-19 15:55:40 +0000

    add back Basic prototests (not sure why these were deleted)

---
+++ tests/Unit/ProtoTests/Basic/out/samples/BasicClient/a_method.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * GENERATED CODE WARNING
+ * This file was automatically generated - do not edit!
+ */
+
+require_once __DIR__ . '/../../../vendor/autoload.php';
+
+// [START basic_generated_Basic_AMethod_sync]
+use Google\ApiCore\ApiException;
+use Testing\Basic\Client\BasicClient;
+use Testing\Basic\Request;
+use Testing\Basic\Response;
+
+/**
+ * Test summary text for AMethod
+ *
+ * This sample has been automatically generated and should be regarded as a code
+ * template only. It will require modifications to work:
+ *  - It may require correct/in-range values for request initialization.
+ *  - It may require specifying regional endpoints when creating the service client,
+ *    please see the apiEndpoint client configuration option for more details.
+ */
+function a_method_sample(): void
+{
+    // Create a client.
+    $basicClient = new BasicClient();
+
+    // Prepare the request message.
+    $request = new Request();
+
+    // Call the API and handle any network failures.
+    try {
+        /** @var Response $response */
+        $response = $basicClient->aMethod($request);
+        printf('Response data: %s' . PHP_EOL, $response->serializeToJsonString());
+    } catch (ApiException $ex) {
+        printf('Call failed with message: %s' . PHP_EOL, $ex->getMessage());
+    }
+}
+// [END basic_generated_Basic_AMethod_sync]
