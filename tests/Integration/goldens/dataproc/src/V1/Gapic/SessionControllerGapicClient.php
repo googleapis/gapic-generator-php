9b76a691b00d4852c2e2f68fcd1b9cc233960bb6
Brent Shaffer <betterbrent@google.com>
2026-04-20 15:51:48 +0000

    update tests

--- tests/Integration/goldens/dataproc/src/V1/Gapic/SessionControllerGapicClient.php
+++ tests/Integration/goldens/dataproc/src/V1/Gapic/SessionControllerGapicClient.php
@@ -798,7 +798,7 @@ class SessionControllerGapicClient
 
     /**
      * Gets the access control policy for a resource. Returns an empty policy
-    if the resource exists and does not have a policy set.
+     * if the resource exists and does not have a policy set.
      *
      * Sample code:
      * ```
@@ -846,10 +846,10 @@ class SessionControllerGapicClient
 
     /**
      * Sets the access control policy on the specified resource. Replaces
-    any existing policy.
-
-    Can return `NOT_FOUND`, `INVALID_ARGUMENT`, and `PERMISSION_DENIED`
-    errors.
+     * any existing policy.
+     *
+     * Can return `NOT_FOUND`, `INVALID_ARGUMENT`, and `PERMISSION_DENIED`
+     * errors.
      *
      * Sample code:
      * ```
@@ -906,12 +906,12 @@ class SessionControllerGapicClient
 
     /**
      * Returns permissions that a caller has on the specified resource. If the
-    resource does not exist, this will return an empty set of
-    permissions, not a `NOT_FOUND` error.
-
-    Note: This operation is designed to be used for building
-    permission-aware UIs and command-line tools, not for authorization
-    checking. This operation may "fail open" without warning.
+     * resource does not exist, this will return an empty set of
+     * permissions, not a `NOT_FOUND` error.
+     *
+     * Note: This operation is designed to be used for building
+     * permission-aware UIs and command-line tools, not for authorization
+     * checking. This operation may "fail open" without warning.
      *
      * Sample code:
      * ```
